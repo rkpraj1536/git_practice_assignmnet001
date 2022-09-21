f1223035605343a5ae2b963894bb97704bd3c7af
Rahul Prajapati <rkpraj1536@gmail.com>
2022-09-21 21:58:55 +0530

    code fix by m1

--- prime.js
+++ prime.js
@@ -1,11 +1,11 @@
 let num=13;
-let counter=0;
+let counters=0;
     for(let i=1;i<=num;i++){
         if(num%i==0){
-            counter++;
+            counters++;
         }
     }
-    if(counter>2){
+    if(counters>2){
         console.log("No");
     }else{
         console.log("Yes");
